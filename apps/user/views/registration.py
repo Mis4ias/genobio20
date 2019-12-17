824a3b04779a2ec2123e5d4976f6ac22cae4ab4d
Rita Lopes <ritalino99@gmail.com>
2019-12-17 16:49:34 -0300

    Populando select da página de registro

--- apps/user/views/registration.py
+++ apps/user/views/registration.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from apps.user import models
 
 def register(request):
-    return render(request, 'user/registration.html')
+    dados = {
+        'estados': models.Estado.objects.all(),
+        'paises' : models.Pais.objects.all(),
+        'tipo_inscricoes' : models.Tipo_Inscricao.objects.all(),
+        'titulos': models.Titulacao.objects.all()
+    }
+
+    return render(request, 'user/registration.html', { 'dados': dados })
     
