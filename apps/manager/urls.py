8a028f2597cfa941dc8d5ee7ecafea37d369ab16
gfaraujo <guilherme.araujo@imd.ufrn.br>
2020-01-28 15:39:38 -0300

    Admin area funcionando básico

--- apps/manager/urls.py
+++ apps/manager/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from apps.manager import views
 
 urlpatterns = [
+    path('admin_area/', views.admin_area, name = 'admin_area' ),
+    path('information/<int:id>', views.information, name='admin_information'),
 
 ]
--- apps/manager/views.py
+++ apps/manager/views.py
@@ -1,3 +1,88 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from datetime import date
+from apps.user import models, send_email
 
-# Create your views here.
+
+def registration_is_available():
+    today = date.today()
+    registration_limit_day = date(2020, 3, 30)
+
+    if today <= registration_limit_day:
+        return True
+    return False
+
+def admin_area( request ):    
+    if request.user.is_authenticated and request.user.is_staff :
+        all_users = models.Usuario.objects.all()
+        all_data_users = []
+        all_data_courses = {}
+
+        for user in all_users:
+            
+
+            data_user = {}
+            data_user["id"] = user.id
+            data_user["name"] = user.nome
+            data_user["email"] = user.user.email
+            try:
+                payment = models.Pagamento.objects.get(usuario = user.id)
+            except models.Pagamento.DoesNotExist:
+                payment = None
+            data_user["payment"] = payment.status.descricao if payment is not None else "-"            
+            
+            
+            all_data_users.append( data_user )            
+
+        return render(request, "manager/admin_area.html", { "users": all_data_users })
+    else:
+        return redirect('user_login')
+
+def information(request, id):
+    if request.user.is_authenticated and request.user.is_staff :
+        try:
+            usuario = models.Usuario.objects.get(pk=id)
+            try:
+                payment = models.Pagamento.objects.get(usuario = usuario.id)
+            except models.Pagamento.DoesNotExist:
+                payment = None
+            
+            data_user = {
+                "id"     : usuario.id,
+                "name"   : usuario.nome,
+                "payment": payment.status.descricao if payment is not None else "-",
+                "observation": payment.observacao if payment is not None else "",        
+                "tel"    : usuario.celular,
+                "email"  : usuario.user.email,
+                "country": usuario.pais.nome,
+                "state"  : usuario.estado.nome,
+                "city"   : usuario.cidade.nome,
+                "zip"    : usuario.cep,
+                "address": usuario.endereco,   
+                "subscription_type" : usuario.tipo_inscricao.tipo,
+                "institution": usuario.instituicao,                
+            }
+
+            user_paid = False
+            show_color = ''
+            if payment is not None:
+                if payment.status.id in [3, 4, 10, 11]:
+                    show_color = 'success'
+                    user_paid = True
+                elif payment.status.id == 1:
+                    show_color = 'warning'
+                    user_paid = True
+            
+
+            return render(request, "manager/information.html", {"dados": data_user, 
+                                                                "payment_status": models.Situacao_de_pagamento.objects.filter(pk__in=[3, 4, 10, 11]),
+                                                                "show_color": show_color,
+                                                                "user_paid": user_paid})
+        except models.Usuario.DoesNotExist:
+            msg = { 
+                'msg': "User do not found.", 
+                'title': "Error" 
+            } 
+            return render(request, 'user/notification.html', { 'msg': msg })
+    else:
+        return redirect('user_login')
